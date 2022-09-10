74fbeaf2e187939a56da0a9e9c3069ece018f92f
조상호 <hyundai_sangho@naver.com>
2022-09-10 12:02:08 +0900

    EmailSender 주석 추가

--- EmailSender/python_app.py
+++ EmailSender/python_app.py
@@ -1,13 +1,20 @@
+# email 보내는데 필요한 모듈 가져오기
 from email.message import EmailMessage
 from app2 import password
 import ssl
 import smtplib
 
+# 이메일 보내는 사람
 email_sender = "chosangho2019@gmail.com"
+# 이메일 패스워드
 email_password = password
+# 이메일 받는 사람
 email_receiver = "teha007@naver.com"
 
+# 이메일 제목 
 subject = "HI SANGHO"
+
+# 이메일 본문 내용
 body = """
   Email send to sangho
 """
